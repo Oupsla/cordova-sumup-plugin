8407dced871c24d10e31e594f6365f60b827d4b4
Nicolas D <nicolas_delperdange@hotmail.com>
2017-08-29 11:58:34 +0200

    feat(sumup): adding payWithToken, login, logout and settings

--- src/android/Sumup.java
+++ src/android/Sumup.java
@@ -15,10 +15,16 @@ import org.json.JSONObject;
 import com.sumup.merchant.api.SumUpAPI;
 import com.sumup.merchant.api.SumUpPayment;
 import com.sumup.merchant.api.SumUpState;
+import com.sumup.merchant.api.SumUpLogin;
 
 import java.util.UUID;
 
 public class Sumup extends CordovaPlugin {
+
+  private static final int REQUEST_CODE_LOGIN = 1;
+  private static final int REQUEST_CODE_PAYMENT = 2;
+  private static final int REQUEST_CODE_PAYMENT_SETTINGS = 3;
+  private static final int REQUEST_CODE_LOGIN_SETTING = 4;
 
   private CallbackContext callback = null;
 
@@ -31,47 +37,47 @@ public class Sumup extends CordovaPlugin {
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
 
-
-    /************ Parse args ******************/
-    Double amount = null;
-    try {
-      amount = Double.parseDouble(args.get(0).toString());
-    } catch (Exception e) {
-      callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, "Can't parse amount"));
-      return false;
-    }
-
-    SumUpPayment.Currency currency = null;
-    try {
-      currency = SumUpPayment.Currency.valueOf(args.get(1).toString());
-    } catch (Exception e) {
-      callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, "Can't parse currency"));
-      return false;
-    }
-
-    String email = "";
-    try {
-      email = args.get(2).toString();
-    } catch (Exception e) {
-      callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, "Can't parse email"));
-      return false;
-    }
-
-    String tel = "";
-    try {
-      tel = args.get(3).toString();
-    } catch (Exception e) {
-      callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, "Can't parse tel"));
-      return false;
-    }
+    String affiliateKey = this.cordova.getActivity().getString(cordova.getActivity().getResources().getIdentifier("SUMUP_API_KEY", "string", cordova.getActivity().getPackageName()));
 
     if (action.equals("pay")) {
+
+      /************ Parse args ******************/
+      Double amount = null;
+      try {
+        amount = Double.parseDouble(args.get(0).toString());
+      } catch (Exception e) {
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, "Can't parse amount"));
+        return false;
+      }
+
+      SumUpPayment.Currency currency = null;
+      try {
+        currency = SumUpPayment.Currency.valueOf(args.get(1).toString());
+      } catch (Exception e) {
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, "Can't parse currency"));
+        return false;
+      }
+
+      String email = "";
+      try {
+        email = args.get(2).toString();
+      } catch (Exception e) {
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, "Can't parse email"));
+        return false;
+      }
+
+      String tel = "";
+      try {
+        tel = args.get(3).toString();
+      } catch (Exception e) {
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, "Can't parse tel"));
+        return false;
+      }
 
       SumUpPayment payment = SumUpPayment.builder()
       // mandatory parameters
       // Your affiliate key is bound to the applicationID entered in the SumUp dashboard at https://me.sumup.com/integration-tools
-      // .affiliateKey(this.cordova.getActivity().getString(cordova.getActivity().getResources().getIdentifier("SUMUP_API_KEY", "string", cordova.getActivity().getPackageName())))
-      .affiliateKey(this.cordova.getActivity().getString(cordova.getActivity().getResources().getIdentifier("SUMUP_API_KEY", "string", cordova.getActivity().getPackageName())))
+      .affiliateKey(affiliateKey)
       .productAmount(amount)
       .currency(currency)
       // optional: add details
@@ -90,18 +96,128 @@ public class Sumup extends CordovaPlugin {
       callback = callbackContext;
       cordova.setActivityResultCallback(this);
 
-      //(suPActivity).se
-      //((MainActivity)this.cordova.getActivity()).setCallback(callbackContext);
-      SumUpAPI.openPaymentActivity(this.cordova.getActivity(), payment, 2);
+      SumUpAPI.openPaymentActivity(this.cordova.getActivity(), payment, REQUEST_CODE_PAYMENT);
       return true;
     }
-    //this.echo("false",callbackContext);
+
+    if (action.equals("payWithToken")) {
+
+      /************ Parse args ******************/
+      Double amount = null;
+      try {
+        amount = Double.parseDouble(args.get(0).toString());
+      } catch (Exception e) {
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, "Can't parse amount"));
+        return false;
+      }
+
+      SumUpPayment.Currency currency = null;
+      try {
+        currency = SumUpPayment.Currency.valueOf(args.get(1).toString());
+      } catch (Exception e) {
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, "Can't parse currency"));
+        return false;
+      }
+
+      String email = "";
+      try {
+        email = args.get(2).toString();
+      } catch (Exception e) {
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, "Can't parse email"));
+        return false;
+      }
+
+      String tel = "";
+      try {
+        tel = args.get(3).toString();
+      } catch (Exception e) {
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, "Can't parse tel"));
+        return false;
+      }
+
+      String token = "";
+      try {
+        token = args.get(4).toString();
+      } catch (Exception e) {
+      }
+
+      SumUpPayment payment = SumUpPayment.builder()
+      // mandatory parameters
+      // Your affiliate key is bound to the applicationID entered in the SumUp dashboard at https://me.sumup.com/integration-tools
+      .affiliateKey(affiliateKey)
+      .accessToken(token)
+      .productAmount(amount)
+      .currency(currency)
+      // optional: add details
+      .receiptEmail(email)
+      .receiptSMS(tel)
+      //.productTitle("Taxi Ride")
+      // optional: Add metadata
+      //.addAdditionalInfo("AccountId", "taxi0334")
+      //.addAdditionalInfo("From", "Paris")
+      //.addAdditionalInfo("To", "Berlin")
+      // optional: foreign transaction ID, must be unique!
+      .foreignTransactionId(UUID.randomUUID().toString()) // can not exceed 128 chars
+      .skipSuccessScreen()
+      .build();
+
+      callback = callbackContext;
+      cordova.setActivityResultCallback(this);
+
+      SumUpAPI.openPaymentActivity(this.cordova.getActivity(), payment, REQUEST_CODE_PAYMENT);
+      return true;
+    }
+
+    if(action.equals("login")) {
+      callback = callbackContext;
+      cordova.setActivityResultCallback(this);
+
+      SumUpLogin sumUplogin = SumUpLogin.builder(affiliateKey).build();
+      SumUpAPI.openLoginActivity(this.cordova.getActivity(), sumUplogin, REQUEST_CODE_LOGIN);
+      return true;
+    }
+
+    if(action.equals("settings")) {
+      callback = callbackContext;
+      cordova.setActivityResultCallback(this);
+
+      SumUpLogin sumUplogin = SumUpLogin.builder(affiliateKey).build();
+      SumUpAPI.openLoginActivity(this.cordova.getActivity(), sumUplogin, REQUEST_CODE_LOGIN_SETTING);
+      return true;
+    }
+
+    if(action.equals("logout")) {
+      this.cordova.getActivity().runOnUiThread(new Runnable() {
+        public void run() {
+          SumUpAPI.logout();
+        }
+      });
+      return true;
+    }
+
     return false;
   }
 
   @Override
   public void onActivityResult(int requestCode, int resultCode, Intent data) {
-    if( requestCode == 2 ) {
+
+    if(requestCode == REQUEST_CODE_LOGIN) {
+      Bundle extra = data.getExtras();
+      int code = extra.getInt(SumUpAPI.Response.RESULT_CODE);
+      String message = extra.getString(SumUpAPI.Response.MESSAGE);
+
+      JSONObject res = new JSONObject();
+      try {
+        res.put("code", code);
+        res.put("message", message);
+      } catch (Exception e) {}
+
+      PluginResult result = new PluginResult(PluginResult.Status.OK, res);
+      result.setKeepCallback(true);
+      callback.sendPluginResult(result);
+    }
+
+    if(requestCode == REQUEST_CODE_PAYMENT) {
 
         Bundle extras = data.getExtras();
 
@@ -125,5 +241,26 @@ public class Sumup extends CordovaPlugin {
         result.setKeepCallback(true);
         callback.sendPluginResult(result);
     }
+
+    if(requestCode == REQUEST_CODE_LOGIN_SETTING) {
+      SumUpAPI.openPaymentSettingsActivity(this.cordova.getActivity(), REQUEST_CODE_PAYMENT_SETTINGS);
+    }
+
+    if(requestCode == REQUEST_CODE_PAYMENT_SETTINGS) {
+      Bundle extra = data.getExtras();
+      int code = extra.getInt(SumUpAPI.Response.RESULT_CODE);
+      String message = extra.getString(SumUpAPI.Response.MESSAGE);
+
+      JSONObject res = new JSONObject();
+      try {
+        res.put("code", code);
+        res.put("message", message);
+      } catch (Exception e) {}
+
+      PluginResult result = new PluginResult(PluginResult.Status.OK, res);
+      result.setKeepCallback(true);
+      callback.sendPluginResult(result);
+    }
+
   }
 }
--- www/sumup.js
+++ www/sumup.js
@@ -1,3 +1,4 @@
+cordova.define("cordova-plugin-sumup.Sumup", function(require, exports, module) {
 var Sumup = function() {};
 
 /*var CountryError = function(code, message) {
@@ -7,6 +8,22 @@ var Sumup = function() {};
 
 Sumup.prototype.pay = function (success, failure, amount, currencycode, email, tel) {
   cordova.exec(success, failure, 'Sumup', 'pay', [amount, currencycode, email, tel]);
+};
+
+Sumup.prototype.payWithToken = function (success, failure, amount, currencycode, email, tel, token) {
+  cordova.exec(success, failure, 'Sumup', 'payWithToken', [amount, currencycode, email, tel, token]);
+};
+
+Sumup.prototype.login = function (success, failure) {
+  cordova.exec(success, failure, 'Sumup', 'login', []);
+};
+
+Sumup.prototype.logout = function (success, failure) {
+  cordova.exec(success, failure, 'Sumup', 'logout', []);
+};
+
+Sumup.prototype.settings = function (success, failure) {
+  cordova.exec(success, failure, 'Sumup', 'settings', []);
 };
 
 if (!window.plugins) {
@@ -19,3 +36,5 @@ if (!window.plugins.sumup) {
 if (module.exports) {
   module.exports = Sumup;
 }
+
+});
